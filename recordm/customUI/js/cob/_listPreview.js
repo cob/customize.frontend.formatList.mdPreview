23374c5edddcab807b7ee24981e99fb040ad94f6
Miguel Mesquita <mm@cob.pt>
2022-12-21 19:45:12 +0000

    fix: only respond to keyword
    also fix version

--- recordm/customUI/js/cob/_listPreview.js
+++ recordm/customUI/js/cob/_listPreview.js
@@ -5,7 +5,7 @@ cob.custom.customize.push(function (core, utils, ui) {
   utils.loadScript("https://cdn.jsdelivr.net/npm/marked/marked.min.js",  () => {
 
     core.customizeAllColumns("*", async function (node, esDoc, fieldInfo) {
-      if (/[$]markdown/.exec(fieldInfo.fieldDefDescription) && node.innerHTML) {
+      if (/[$]markdown.*[$]listPreview/.exec(fieldInfo.fieldDefDescription) && node.innerHTML) {
         node.classList.add("dollarMarkdownCell");
         node.innerHTML = " <div class='dollarMarkdownText'> preview </div>"
                         +" <div class='dollarMarkdownPreview'>"
