df197d0cd8e35cd0f4b6a795877e4072050767ae
Miguel Mesquita <mm@cob.pt>
2023-01-05 19:29:08 +0000

    feat: add support for html in markdown

--- recordm/customUI/js/cob/_mdPreview.js
+++ recordm/customUI/js/cob/_mdPreview.js
@@ -2,11 +2,20 @@ cob.custom.customize.push(function (core, utils, ui) {
   utils.loadScript("https://cdn.jsdelivr.net/npm/marked/marked.min.js",  () => {
 
   core.customizeAllColumns("*", (node, esDoc, colDef) => {
+    function htmlDecode(input){
+      var e = document.createElement('div');
+      e.innerHTML = input;
+      return e.childNodes[0].nodeValue;
+    }
+
     if (/[$]markdown.*[$]style\[([^,]+,)*mdPreview(,[^,]+)*\]/.exec(colDef.fieldDefDescription) && node.innerHTML) {
+      debugger
+      marked.setOptions({xhtml:false})
+
       node.classList.add("dollarMarkdownCell");
       node.innerHTML = " <div class='dollarMarkdownText'> preview </div>"
                       +" <div class='dollarMarkdownPreview'>"
-                      +    marked.parse(node.innerHTML)
+                      +    marked.parse(htmlDecode(node.innerHTML))
                       +" </div>"
       }
     });
